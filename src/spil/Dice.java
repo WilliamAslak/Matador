d44c562818f8844c52aa4954d3d3d288b5c34ddb
Andrey <flere2@hotmail.com>
2022-10-24 15:38:08 +0200

    Har bare committed dice+rafflecup klasserne fra cdio1 til genbrug

---
+++ src/spil/Dice.java
@@ -0,0 +1,16 @@
+package spil;
+
+public class Dice {
+
+    private int faceValue;
+
+    public Dice(){}
+
+    public void setFaceValue(int x){
+        faceValue = x;
+    }
+    public int getFaceValue(){
+        return faceValue;
+    }
+
+}
--- src/spil/Game.java
+++ src/spil/Game.java
@@ -1,4 +1,17 @@
 package spil;
 
 public class Game {
+    public static void main(String[] args) {
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Rafflecup rc = new Rafflecup();
+
+        for (int i = 0; i < 100; i++) {
+            rc.roll(d1,d2);
+            System.out.println(rc.getSum(d1,d2));
+        }
+
+    }
+
 }
