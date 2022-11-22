72838a193ab069ff7e7fd292d268a0e140724c7b
Jakob <Sezio@gmail.com>
2022-11-22 11:32:45 +0100

    Made skeleton of chance cards

---
+++ src/main/java/ChanceCards/ChanceCard.java
@@ -0,0 +1,13 @@
+package ChanceCards;
+
+import spil.Game;
+import spil.Player;
+
+abstract public class ChanceCard {
+    String description;
+
+    public ChanceCard(String description) {
+        this.description = description;
+    }
+    abstract public void process(Game game, Player player);
+}
---
+++ src/main/java/ChanceCards/MoveToStart.java
@@ -0,0 +1,15 @@
+package ChanceCards;
+
+import spil.Game;
+import spil.Player;
+
+public class MoveToStart extends ChanceCard {
+
+    public MoveToStart(String description) {
+        super(description);
+    }
+
+    public void process(Game in, Player with){
+
+    }
+}
